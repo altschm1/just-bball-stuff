9a2b816bb50fbd8ec7d0677077da7231d58d2fb3
Michael Altschuler <altschm1@tcnj.edu>
2021-09-19 23:55:33 -0400

    added about section

--- justbballstuff/homepage/views.py
+++ justbballstuff/homepage/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'homepage/home.html', {'title_head': 'Just BBall Stuff', 'home':True})
+
+def about(request):
+    return render(request, 'homepage/about.html', {'title_head': 'Just BBall Stuff: About', 'about':True})
